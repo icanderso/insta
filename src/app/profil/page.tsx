bb2aad6536a6b0f7ef8fcc02024da5a517f2b1d0
icanderso <isabellacristianaanderson@gmail.com>
2024-09-24 12:30:04 +0200

    routing update

---
+++ src/app/profil/page.tsx
@@ -0,0 +1,16 @@
+// src/app/profil/page.tsx
+
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: "Profil | insta "};
+
+export default function ProfileList() {
+
+  return (
+    <Container>
+     <Typography> Zoznam profilov </Typography>
+    </Container>
+  );
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+// src/app/profil/[id]/page.tsx
+
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+
+export const metadata = { title: "Detail profilu | insta "};
+
+export default function ProfilDetail() {
+
+  return (
+    <Container>
+     <Typography> Detail profilu </Typography>
+    </Container>
+  );
+}
