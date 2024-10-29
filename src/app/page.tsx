d0c11a5597c34d6c766f519da06adf7bbd60e16e
icanderso <isabellacristianaanderson@gmail.com>
2024-10-29 13:01:08 +0100

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 
 // src/app/page.tsx
 
-import AuthorizedView from "@/sections/AuthorizedView";
-import NonAuthorizedView from "@/sections/NonAuthorizedView";
+
 import React from 'react';
-import { useRouter } from "next/navigation";
+
 const HomePage = () => {
   return (
     <div>
